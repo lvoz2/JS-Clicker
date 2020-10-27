dc6e67080311d920eee09699e53a36c02885ad59
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2020-10-27 12:22:25 +1100

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,98 @@
+// Save Variables
+
+// Load Variables
+
+var coinfile;
+var coalfile;
+var ironfile;
+var copperfile;
+var rubberfile;
+
+// Resource Variables
+
+var resourceVisible = 'stone';
+
+// Coins
+
+var coins = 0;
+var coinsspeed = 0;
+var coinsgatherers = 0;
+
+// Coal
+
+var coal = 0;
+var coalvalue;
+var coalspeed = 0;
+var coalgatherers = 0;
+
+// Stone
+
+var stone = 0;
+var stonevalue;
+var stonespeed = 0;
+var stonegatherers = 0;
+
+// Iron
+
+var iron = 0;
+var ironvalue;
+var ironspeed = 0;
+var irongatherers = 0;
+
+// Copper
+
+var copper = 0;
+var coppervalue;
+var copperspeed = 0;
+var coppergatherers = 0;
+
+// Rubber
+
+var rubber = 0;
+var rubbervalue;
+var rubberspeed = 0;
+var rubbergatherers = 0;
+
+// Button Variables
+
+var makecopperbutton;
+var gencopperbutton;
+var gencopperbutton1;
+var makerubberbutton;
+var genrubberbutton;
+var genrubberbutton1;
+var readyiron;
+var readycopper;
+var readyrubber;
+var genironbutton1;
+var genironbutton;
+var makeironbutton;
+var makeironbuttonclickedsuccess;
+var makecopperbuttonclickedsuccess;
+var makerubberbuttonclickedsuccess;
+
+// Element Variables
+
+var divbuyiron = document.getElementById("buyiron");
+var divbuycopper = document.getElementById("buycopper");
+var divbuyrubber = document.getElementById("buyrubber");
+var triron = document.getElementById("triron");
+var trcopper = document.getElementById("trcopper");
+var trrubber = document.getElementById("trrubber");
+var pcoins = document.getElementById("coins")
+var pstone = document.getElementById("stone")
+var pcoal = document.getElementById("coal")
+var piron = document.getElementById("iron")
+var pcopper = document.getElementById("copper")
+var prubber = document.getElementById("rubber")
+var divload = document.getElementById("load");
+var divnav = document.getElementById("nav");
+var h2resourcetype = document.getElementById("resourcetype");
+var h3selltype = document.getElementById("selltype");
+var h3storagetype = document.getElementById("storagetype");
+var presourcedefine = document.getElementById("resourcedefine");
+var pselldefine = document.getElementById("selldefine");
+var pstoragedefine = document.getElementById("storagedefine");
+var btngainresource = document.getElementById("gainresource");
+var btnsellresource = document.getElementById("sellresource");
+var btngainstorage = document.getElementById("gainstorage");
